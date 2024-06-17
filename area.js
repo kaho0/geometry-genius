15f5a616388d6c391587a109d7ab4a6cdb773c73
kaho0 <kahonbintezaman@gmail.com>
2024-06-18 00:15:49 +0600

    allcalculator

--- area.js
+++ area.js
@@ -12,4 +12,62 @@ function calculateTriangleArea(){
 
     const area=0.5*base*height;
    console.log(area);
+   const areaSpan=document.getElementById('triangle-area');
+   areaSpan.innerText=area;
 }
+function calculateRectangleArea()
+{
+    const widthField=document.getElementById('rectangle-width');
+    const widthValueText=widthField.value;
+    const width=parseFloat(widthValueText);
+    console.log(width);
+
+    const lengthField=document.getElementById('rectangle-length');
+    const lengthValueText=lengthField.value;
+    const length=parseFloat(lengthValueText);
+    console.log(length);
+
+    const area=width*length;
+    console.log(area);
+
+    const rectangleAreaSpan=document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText=area;
+}
+
+//reusable
+function calculateParallelogramArea()
+{
+   const base=getInputValue('parallelogram-base');
+  const length=getInputValue('parallelogram-height');
+  const area=base*length;
+  getInnerText('parallelogram-area',area);
+
+}
+function calculateEllipseArea()
+{
+   const base=getInputValue('ellipse-major-rad');
+  const length=getInputValue('ellipse-minor-rad');
+  const area= 3.14*base*length;
+  getInnerText('ellipse-area',area);
+
+}
+function calculateRhombusArea()
+{
+   const base=getInputValue('rhombus-d1');
+  const length=getInputValue('rhombus-d2');
+  const area= 0.5*base*length;
+  getInnerText('rhombus-area',area);
+
+}
+
+
+function getInputValue(fieldId){
+    const inputField=document.getElementById(fieldId);
+    const inputValueText=inputField.value;
+    const value=parseFloat(inputValueText);
+    return value;
+}
+ function getInnerText(fieldSpan,area){
+     const areaSpan=document.getElementById(fieldSpan);
+     areaSpan.innerText=area; 
+ }
